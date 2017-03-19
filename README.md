24bb1ac521392bc96c76b223307bb61fece9f534
Srujan Kumar Aakurathi <srujan.neo@gmail.com>
2017-03-19 22:37:24 +0530

    Unipolar Coding Scheme in Matlab

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Unipolar-Coding-Scheme-in-Matlab
+
+
+Uni Polar Coding scheme is a type of line coding (a technique of converting digital data to digital signals). In uni polar coding scheme simply a ‘0’ bit represent a zero voltage level and ‘1’ bit represents a positive 1 voltage level.
---
+++ UnipolarCodingScheme.m
@@ -0,0 +1,28 @@
+function [ output_args ] = UnipolarCodingScheme( input_args )
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+x=[1 0 1 1 0 1];
+
+y=zeros(1,length(x));
+
+n=5;
+
+for i=1:length(x)
+
+   if(x(i)==1)
+
+       y(i*n:(i+1)*n)=1;
+
+   else
+
+       y(i*n:(i+1)*n)=0;
+
+   end
+
+end
+
+stairs(y)
+
+
+end
+
